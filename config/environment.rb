4a3e1cdfc800c2e91823f3d1821975a656429099
conrad <conrad@d7bf5ae1-4f24-0410-b5c4-8995abff36bd>
2006-12-15 06:12:06 +0000

    Added configuration management for dev and prod environments
    git-svn-id: file:///svn/gracelist@7 d7bf5ae1-4f24-0410-b5c4-8995abff36bd

--- config/environment.rb
+++ config/environment.rb
@@ -57,7 +57,7 @@ NOTIFY_FREQUENCY = 2
 
 c = YAML::load(File.open("#{RAILS_ROOT}/config/config.yml"))
 ActionMailer::Base.raise_delivery_errors = true
-ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.delivery_method = c[RAILS_ENV]['email']['delivery_method']
 ActionMailer::Base.server_settings = {
    :address => c[RAILS_ENV]['email']['server'],
    :port => c[RAILS_ENV]['email']['port'],
