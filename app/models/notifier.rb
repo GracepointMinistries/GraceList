241b8a3578143100e48a047bd31906ff57300f5c
conrad <conrad@d7bf5ae1-4f24-0410-b5c4-8995abff36bd>
2006-12-18 09:09:25 +0000

    Changed to koinonia
    git-svn-id: file:///svn/gracelist@15 d7bf5ae1-4f24-0410-b5c4-8995abff36bd

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -3,7 +3,7 @@ class Notifier < ActionMailer::Base
   def newsletter(body, recent_items, sent_at = Time.now)    
     @subject    = '[GraceList] ' + sent_at.strftime('%A, %m/%d') + ' - ' + recent_items + ' new posts'
     @body       = {"body" => body}
-    @recipients = 'chu.conrad@gmail.com'
+    @recipients = 'koinonia@gpmail.org'
     @from       = 'gracelist@gpmail.org'
     @sent_on    = sent_at
     @headers    = {}
