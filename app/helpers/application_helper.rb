0d2ed20e93eca39496c09f40730e622a46965498
conrad <conrad@d7bf5ae1-4f24-0410-b5c4-8995abff36bd>
2006-12-19 00:30:58 +0000

    Reverted to 3.0.3 of RedCloth, see #142
    git-svn-id: file:///svn/gracelist@18 d7bf5ae1-4f24-0410-b5c4-8995abff36bd

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,3 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def tlize(text)
-    text = RedCloth.new(text)
-    return text.to_html
-  end
 end
