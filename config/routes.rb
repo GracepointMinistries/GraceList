7861a96eacfd7756982e02e0c1501de8c3f911b5
conrad <conrad@d7bf5ae1-4f24-0410-b5c4-8995abff36bd>
2009-02-11 00:51:20 +0000

    Updated RSS
    git-svn-id: file:///svn/gracelist@47 d7bf5ae1-4f24-0410-b5c4-8995abff36bd

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,11 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed by hooking up '' 
   # -- just remember to delete public/index.html.
-  map.connect '', :controller => "posts"
+  map.connect '', :controller => "posts", :action => 'index'
 
   map.posts '/:id', :controller => 'posts', :action => 'show', :id => /\d+/ 
+  map.feed '/feed', :controller => 'posts', :action => 'rss' 
+	
 
   # Allow downloading Web Service WSDL as a file with an extension
   # instead of a file named 'wsdl'
