a163a7da8819f10d2805e82e35c8bb6dbbd3ffc4
Conrad Chu <conradchu@conradchu.com>
2017-03-10 11:21:29 -0800

    Upgrade to Rails 5, wiped out old files

--- db/migrate/003_add_for_sale_category.rb
+++
@@ -1,9 +0,0 @@
-class AddForSaleCategory < ActiveRecord::Migration
-  def self.up
-    Category.create(:name => "For Sale")
-  end
-
-  def self.down
-    Category.find_by_name('For Sale').destroy
-  end
-end
