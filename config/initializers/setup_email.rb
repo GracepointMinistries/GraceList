2d721007ae99ab31484ff29e11e79a03d9ddb674
Conrad Chu <conradchu@conradchu.com>
2017-03-13 12:19:03 -0700

    Moved mail config to production

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -1,13 +1,15 @@
 # Set up connection to Mailer
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  address:        Settings.mailer.address,
-  port:           Settings.mailer.port,
-  domain:         Settings.mailer.domain,
-  authentication: :plain,
-  user_name:      Settings.mailer.user_name,
-  password:       Settings.mailer.password
-}
+if Rails.env.production?
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address:        Settings.mailer.address,
+    port:           Settings.mailer.port,
+    domain:         Settings.mailer.domain,
+    authentication: :plain,
+    user_name:      Settings.mailer.user_name,
+    password:       Settings.mailer.password
+  }
+end
 
 # Intercept all email in development
 require 'development_mail_interceptor'
